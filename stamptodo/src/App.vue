f3b3f1445d84d65be4faa7cfd75dbce78b9a3426
Azusa Okamoto <curiousazunas@gmail.com>
2020-09-11 18:45:39 +0900

    削除機能と完了機能の作成

--- stamptodo/src/App.vue
+++ stamptodo/src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id='app'>
     <h1>タスクスタンプラリー</h1>
-    <form>
-      <input type='text' />
+    <form v-on:submit.prevent='onclick'>
+      <input type='text' id='title' v-model='title' required />
       <input type='submit' value='ADD' />
     </form>
-    <ul>
+    <ul v-for='( todo, index ) in todos' :key='index'>
       <li>
-        <input type='button' value='DONE' />
-        <input type='button' value='DELETE' />
+        {{ todo.d }}
+        {{ todo.title }}
+      </li>
+        <input type='button' value='DONE' v-on:click='doneTodo(todo.d)' />
+        <input type='button' value='DELETE' v-on:click='deleteTodo(todo.d)' />
+    </ul>
+    <ul v-for='(done, index) of dones' :key='index'>
+      <li>
+        {{ done }}
       </li>
     </ul>
   </div>
@@ -18,6 +25,37 @@
 
 export default {
   name: 'App',
+  computed: {
+    todos() {
+      return this.$store.getters.todos;
+    },
+    dones() {
+      return this.$store.getters.dones;
+    }
+  },
+  data() {
+    return {
+      d: '',
+      title: ''
+    }
+  },
+  methods: {
+    onclick() {
+      this.$store.commit('addTodo', {
+        todo: {
+          d: new Date(),
+          title: this.title // 入力欄に入力された値。
+        }
+      });
+      this.title = ''; // 入力部は空欄に戻す。
+    },
+    deleteTodo(d) {
+      this.$store.commit('deleteTodo', d);
+    },
+    doneTodo(d) {
+      this.$store.commit('doneTodo', d);
+    }
+  }
 }
 </script>
 
@@ -29,5 +67,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ul li {
+  list-style: none;
 }
 </style>
--- stamptodo/src/store/index.js
+++ stamptodo/src/store/index.js
@@ -5,8 +5,36 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    todos: [
+      {
+        d: new Date(),
+        title: 'PHP'
+      }
+    ],
+    dones: [
+    ]
+  },
+  getters: {
+    todos(state) {
+      return state.todos;
+    },
+    dones(state) {
+      return state.dones;
+    }
   },
   mutations: {
+    addTodo(state, payload) {
+      state.todos.push(payload.todo);
+    },
+    deleteTodo(state, d) {
+      let index = state.todos.findIndex(todo => todo.d === d);
+      state.todos.splice(index, 1);
+    },
+    doneTodo(state, d) {
+      let index = state.todos.findIndex(todo => todo.d === d);
+      let doneItem = state.todos.splice(index, 1);
+      state.dones.push(doneItem);
+    }
   },
   actions: {
   },
